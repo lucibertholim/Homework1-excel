c1c270958e755330189e3b395eec25af03a3a097
lucibn <43211666+lucibertholim@users.noreply.github.com>
2018-10-30 12:45:37 -0400

    Editing to complete homework

--- Python-challenge-03/PyBoss/Main.py
+++ Python-challenge-03/PyBoss/Main.py
@@ -1,5 +1,61 @@
 import os
 import csv
+import string
+from datetime import datetime
+
+us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+}
+
 
 #opening csv file
 
@@ -8,7 +64,25 @@ new_disctionary={}
 with open (csvpath, "r", newline="") as csvfile:
     csvreader=csv.DictReader(csvfile)
     for row in csvreader:
-        print(row['Emp ID'], row['Name'], row['SSN'], row['State'])
-        
-        
+       (row["DOB"]) = datetime.strptime((row["DOB"]), '%Y-%m-%d')
+       DOB2 = (row["DOB"]).strftime('%m/%d/%Y')
+       x=(row["SSN"])
+       SSN2= ("***-**"+str(x[6:]))
+       State2=us_state_abbrev[(row["State"])]
+       employee_dictionary = {
+                   "Emp ID": row["Emp ID"],
+                   "First Name": row["Name"].split(" ")[0].strip(),
+                   "Last Name": row["Name"].split(" ")[1].strip(),
+                   "DOB": str(DOB2),
+                   "SSN": str(SSN2),
+                   "State": str(State2),
+                   }        
        
+
+       with open("employee_data2.csv", 'w') as csvfile:
+           fieldnames = ["Emp ID", "First Name", "Last Name", "DOB", "SSN", "State"]
+           writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+       
+           writer.writeheader()
+           
+           writer.writerow(employee_dictionary)
