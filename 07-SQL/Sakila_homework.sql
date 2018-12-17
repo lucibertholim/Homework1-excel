f0d91c025b2420309ead1d459be2e3e0dd7ebeba
lucibn <43211666+lucibertholim@users.noreply.github.com>
2018-12-16 22:05:28 -0500

    Update Sakila_homework.sql

--- 07-SQL/Sakila_homework.sql
+++ 07-SQL/Sakila_homework.sql
@@ -32,6 +32,7 @@ FROM sakila.country
 WHERE country IN ('Afghanistan', 'Bangladesh', 'China');
 
 # 3a. You want to keep a description of each actor. You don't think you will be performing queries on a description, so create a column in the table `actor` named `description` and use the data type `BLOB` (Make sure to research the type `BLOB`, as the difference between it and `VARCHAR` are significant).
+UPDATE
 
 # 3b. Very quickly you realize that entering descriptions for each actor is too much effort. Delete the `description` column.
 
